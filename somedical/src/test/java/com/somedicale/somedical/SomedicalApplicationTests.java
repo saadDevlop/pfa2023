a479bfdea5968be6f003bca35c2dccbf3552dca4
saadDevlop <105066118+saadDevlop@users.noreply.github.com>
2023-05-07 18:13:53 +0100

    ajouter projet spring

---
+++ somedical/src/test/java/com/somedicale/somedical/SomedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somedicale.somedical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomedicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
